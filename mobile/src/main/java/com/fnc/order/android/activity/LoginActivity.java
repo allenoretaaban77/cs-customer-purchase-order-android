0ea425c5d8a6be298f6023568c9994a7836ea99e
Allen Oreta Aban <aban.allen@gmail.com>
2019-05-10 14:20:57 +0800

    update

--- mobile/src/main/java/com/fnc/order/android/activity/LoginActivity.java
+++ mobile/src/main/java/com/fnc/order/android/activity/LoginActivity.java
@@ -99,8 +99,10 @@ public class LoginActivity extends BaseActivity implements VolleyCallback {
         tvVersion.setText(Helper.getVersion(ctx, this));
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        usernameText.setText("19130");
-        passwordEText.setText("nathaniels@1994");
+//        usernameText.setText("19130");
+//        passwordEText.setText("nathaniels@1994");
+        usernameText.setText("aban.allen@yahoo.com");
+        passwordEText.setText("7777777");
     }
 
     private void initListeners(){
